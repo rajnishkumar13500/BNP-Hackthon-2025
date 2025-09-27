4253197d86b9f50e4aea83091d64b674fff68b2b
khajavali <shaikkhajavali0001@gmail.com>
2025-09-28 02:36:47 +0530

    fe dine

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+});
